8892b1c1b6d3945e3eb0b2b21d674ebdbec9cd12
labry <justin.labry@gmail.com>
2020-08-13 13:45:46 +0900

    Work on article password

--- third/forms.py
+++ third/forms.py
@@ -32,19 +32,38 @@ class ReviewForm(ModelForm):
 class RestaurantFrom(ModelForm):
     class Meta:
         model = Restaurant
-        fields = ['name', 'address']
+        fields = ['name', 'address', 'image', 'password']
         labels = {
             'name': _('Name:'),
             'address': _('Address:'),
+            'image': _('imge url'),
+            'password': _('article password:'),
         }
 
         help_texts = {
             'name': _('input your name.'),
             'address': _('input your address.'),
+            'image': _('input your image url:'),
+            'password': _('input your article password:'),
+        }
+
+        widgets = {
+            'password': forms.PasswordInput(),
         }
 
         error_messages = {
             'name': {
                 'max_length': _('Too long name. Type within 30 characters.')
-            }
+            },
+            'image': {
+                'max_length': _('Too long url. Type within 500 characters.')
+            },
+            'password': {
+                'max_length': _('Too long password. Type within 20 characters.')
+            },
         }
+
+class UpdateRestaurantForm(RestaurantFrom):
+    class Meta:
+        model = Restaurant
+        exclude = ['password']
--- third/views.py
+++ third/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from third.models import Restaurant, Review
 from django.core.paginator import Paginator
-from third.forms import RestaurantFrom, ReviewForm
+from third.forms import RestaurantFrom, ReviewForm, UpdateRestaurantForm
 from django.http import HttpResponseRedirect
+from django.db.models import Count, Avg
 
 # Create your views here.
 def list(request):
-    restaurants = Restaurant.objects.all()
+    restaurants = Restaurant.objects.all().annotate(reviews_count=Count('review'))\
+        .annotate(average_point=Avg('review__point'))
 
     paginator = Paginator(restaurants, 4)
 
@@ -39,8 +41,10 @@ def update(request, restaurant_id):
     if request.method == 'POST' and restaurant_id is not None:
         # item = Restaurant.objects.get(pk=request.POST.get('id'))
         item = get_object_or_404(Restaurant, pk=restaurant_id)
-        form = RestaurantFrom(request.POST, instance=item)
-        if form.is_valid():
+        password = request.POST.get('password', '')
+
+        form = UpdateRestaurantForm(request.POST, instance=item)
+        if form.is_valid() and password == item.password:
             new_item = form.save()
     elif request.method == 'GET':
         # item = Restaurant.objects.get(pk=request.GET.get('id')) # third/update?id=2
@@ -59,10 +63,17 @@ def detail(request, restaurant_id):
 
 
 def delete(request, restaurant_id):
-    if request.method == 'GET' and restaurant_id is not None:
-        item = get_object_or_404(Restaurant, pk=restaurant_id)
-        item.delete()
-    return HttpResponseRedirect('/third/list/')
+    item = get_object_or_404(Restaurant, pk=restaurant_id)
+    if request.method == 'POST' and 'password' in request.POST:
+        if item.password == request.POST.get('password') or item.password is None:
+            item.delete()
+            return redirect('list')
+        return redirect('restaurant-detail', restaurant_id=restaurant_id)
+    return render(request, 'third/delete.html', {'item':item})
+
+    # if request.method == 'GET' and restaurant_id is not None:
+    #     item.delete()
+    # return HttpResponseRedirect('/third/list/')
 
 def review_create(request, restaurant_id):
     #print("!!!!!!")
@@ -76,3 +87,24 @@ def review_create(request, restaurant_id):
     item = get_object_or_404(Restaurant, pk=restaurant_id)
     form = ReviewForm(initial={'restaurant':item})
     return render(request, 'third/review_create.html', {'form':form, 'item':item })
+
+
+def review_delete(request, restaurant_id, review_id):
+    #print("@@@")
+    item = get_object_or_404(Review, pk=review_id)
+    item.delete()
+    #print('###')
+    return redirect('restaurant-detail', restaurant_id=restaurant_id)
+
+def review_list(request):
+    reviews = Review.objects.all().select_related().order_by('-created_at')
+    paginator = Paginator(reviews, 10)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context = {
+        'reviews': items
+    }
+
+    return render(request, 'third/review_list.html', context)
